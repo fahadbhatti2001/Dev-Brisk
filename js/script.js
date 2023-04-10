a5ff7966902f631a8c9e7e854bed4e8c9a447403
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2023-04-10 21:45:13 +0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('anim-app'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets2.lottiefiles.com/private_files/lf30_ue6h0nrr.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('anim-web'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets4.lottiefiles.com/packages/lf20_mmb34ca2.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('anim-ai'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets4.lottiefiles.com/packages/lf20_rofn3uw2.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('anim-cloud'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets3.lottiefiles.com/packages/lf20_7u7w338y.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('home-screen'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets4.lottiefiles.com/packages/lf20_tnqcclxh.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('about-screen'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets3.lottiefiles.com/packages/lf20_xc4vliq1.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('contact-screen'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets6.lottiefiles.com/packages/lf20_eawskgf5.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document. getElementById('service-screen'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets7.lottiefiles.com/packages/lf20_pgk6z25d.json"
+})
